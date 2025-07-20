5c04e9ccd0d95137d203022e2ed0411a3753161d
Abhinav Sharma <157093188+abhinavshar17@users.noreply.github.com>
2025-07-20 20:01:22 +0530

    Time: 1 ms (99.45%) | Memory: 42.5 MB (80.55%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,25 +1,35 @@
 class Solution {
     public int longestValidParentheses(String s) {
-       Stack<Integer> stack=new Stack<>();
-       stack.push(-1);
-       int maxl=0;
-       for(int i=0;i<s.length();i++){
-        char ch=s.charAt(i);
+        int left = 0, right = 0, max = 0;
 
-        if(ch=='('){
-            stack.push(i);
+        // Left to Right scan
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
+                left++;
+            else
+                right++;
+
+            if (left == right)
+                max = Math.max(max, left * 2);
+            else if (right > left)
+                left = right = 0;
         }
-        else{
-            stack.pop();
+
+        left = right = 0;
+
+        // Right to Left scan
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(')
+                left++;
+            else
+                right++;
+
+            if (left == right)
+                max = Math.max(max, left * 2);
+            else if (left > right)
+                left = right = 0;
         }
-        if(stack.isEmpty()){
-            stack.push(i);
-        
-        }
-        else{
-            maxl=Math.max(maxl,i-stack.peek());
-        }
-       } 
-       return maxl;
+
+        return max;
     }
 }
