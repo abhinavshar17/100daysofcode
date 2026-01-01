d23e4b09ba6fa14762e04491523a323baa4a2ba2
Abhinav Sharma <157093188+abhinavshar17@users.noreply.github.com>
2026-01-01 16:46:15 +0530

    Time: 151 ms (37.91%) | Memory: 298.4 MB (24.51%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,33 +1,28 @@
 class Solution {
-
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        // Build graph
-        ArrayList<Integer>[] graph = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
+        ArrayList<ArrayList<Integer>>graph=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            graph.add(new ArrayList<>());
         }
-
-        for (int[] e : edges) {
-            graph[e[0]].add(e[1]);
-            graph[e[1]].add(e[0]);
+        for(int i=0;i<edges.length;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
         }
-
-        boolean[] visited = new boolean[n];
-        return dfs(source, destination, graph, visited);
+        boolean visited[]=new boolean[n];
+        return dfs(graph,source,destination,visited);
     }
-
-    private boolean dfs(int src, int dest, ArrayList<Integer>[] graph, boolean[] visited) {
-        if (src == dest) return true;
-
-        visited[src] = true;
-
-        for (int nei : graph[src]) {
-            if (!visited[nei]) {
-                if (dfs(nei, dest, graph, visited)) {
-                    return true;
-                }
+     public boolean dfs(ArrayList<ArrayList<Integer>> graph,int source,int destination,boolean visited[]){
+        if(source==destination)return true;
+        visited[source]=true;
+        for(int neighbour: graph.get(source)){
+            if(!visited[neighbour]){
+                    boolean abc=dfs(graph,neighbour,destination,visited);
+                        if(abc==true) return true;
+                    
             }
         }
         return false;
-    }
+     }
 }
